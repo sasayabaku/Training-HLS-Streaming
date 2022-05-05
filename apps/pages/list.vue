23c89c45a05cab496afb1428e56dd6c3b1982bb4
sasayabaku <sasayabaku@yahoo.co.jp>
2022-05-05 23:25:43 +0900

    Dev : List shuffle

--- apps/pages/list.vue
+++ apps/pages/list.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <vs-row vs-justify="center">
-            <vs-col class="card_row" type="flex" vs-justify="center" vs-w="3" vs-sm="6" vs-xs="12" v-for="item in items" :key="item.item">
+            <vs-col class="card_row" type="flex" vs-justify="center" vs-w="3" vs-sm="6" vs-xs="12" v-for="item in this.shuffle(items)" :key="item.item">
                 <nuxt-link :to="item.url">
                     <vs-card class="cardx">
                         <div slot="header"><h3>{{item.title}}</h3></div>
@@ -22,8 +22,16 @@ export default {
     data() {
         return {
             items: [
-                {id:1, title: "Example", url: "/player/?url=example", src: "/img/example/0001.png"}
             ]
+        }
+    },
+    methods: {
+        shuffle: function(array) {
+            for (let i = array.length - 1; i >= 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [array[i], array[j]] = [array[j], array[i]];
+            }
+            return array;
         }
     }
 }
